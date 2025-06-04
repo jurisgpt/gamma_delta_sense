aa18590985f5cd276a6944072bb7515fce03e90a
jurisgpt <126611532+jurisgpt@users.noreply.github.com>
2025-06-04 07:34:01 -0700

    Fix: Replace entire conf.py with working alabaster theme

--- docs/conf.py
+++ docs/conf.py
@@ -1,22 +1,16 @@
 # Configuration file for the Sphinx documentation builder.
 
-# -- Project information -----------------------------------------------------
 project = 'Gamma Delta Sense'
 copyright = '2025, JurisGPT'
 author = 'JurisGPT'
-
 version = '1.0.0'
 release = '1.0.0'
 
-# -- General configuration ---------------------------------------------------
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',  # For Google-style docstrings
+    'sphinx.ext.napoleon',
 ]
 
 templates_path = ['_templates']
@@ -24,41 +18,22 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 language = 'en'
 master_doc = 'index'
 source_suffix = '.rst'
-source_encoding = 'utf-8-sig'
 
-# -- Options for HTML output -------------------------------------------------
-# Using Read the Docs theme
-html_theme = 'sphinx_rtd_theme'
+# CRITICAL: Use built-in theme (NOT sphinx_rtd_theme)
+html_theme = 'alabaster'
+html_static_path = []
 
-# Theme options
 html_theme_options = {
-    'navigation_depth': 4,
-    'collapse_navigation': False,
-    'sticky_navigation': True,
-    'includehidden': True,
-    'titles_only': False
+    'description': 'Immunology Knowledge Base Development & Monitoring System',
+    'github_user': 'jurisgpt',
+    'github_repo': 'gamma_delta_sense',
+    'github_banner': True,
+    'show_powered_by': False,
 }
-
-html_static_path = ['_static']  # Create this directory if needed
-
-# Add any paths that contain custom static files (such as style sheets)
-# They are copied after the builtin static files
-html_css_files = [
-    'custom.css',
-]
-
-# Add any paths that contain custom themes here, relative to this directory
-html_theme_path = []
 
 html_show_sphinx = False
 html_show_copyright = True
-htmlhelp_basename = 'GammaDeltaSensedoc'
 
-# -- Options for intersphinx extension ---------------------------------------
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None),
 }
-
-# -- Options for todo extension ----------------------------------------------
-todo_include_todos = True
-
